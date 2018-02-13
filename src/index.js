aa8e7f153f44951d02840019bd020439e90aba87
UnnamedHero <hero.nospam@gmail.com>
2018-02-13 11:32:53 +0300

    extend greetings logic in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import readlineSync from 'readline-sync';
 
-export default() => {
+export default(gameText) => {
   console.log('Welcome to the Brain Games!');
+  if (gameText) {
+    console.log(gameText);
+  }
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
