97328440641870ebf53c8ac7d6a738111510baad
UnnamedHero <hero.nospam@gmail.com>
2018-02-13 13:04:55 +0300

    separate default func into two separate functions in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import readlineSync from 'readline-sync';
 
-export default(gameText) => {
+export const showGreetings = (gameText) => {
   console.log('Welcome to the Brain Games!');
   if (gameText) {
-    console.log(gameText);
+    console.log(`${gameText}\n`);
   }
+};
+
+export const getUserName = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   return userName;
